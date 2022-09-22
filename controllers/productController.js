405c835a147d921cd1f3d570bf21b3cf934fc878
Viorel <viorel_popa89@yahoo.com>
2022-09-22 12:29:09 +0300

    add a product using body from post data

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,7 @@
 const Product = require("../models/productModel");
 
+const { getPostData } = require("../utils");
+
 //@desc Gets All Products
 //@route GET /api/products
 async function getProducts(req, res) {
@@ -35,13 +37,18 @@ async function getProduct(req, res, id) {
 //@route POST /api/products
 async function createProduct(req, res) {
   try {
+    const body = await getPostData(req);
+
+    const { title, description, price } = JSON.parse(body);
+
     const product = {
-      title: "Test Product",
-      description: "THis is my product",
-      price: 100,
+      title,
+      description,
+      price,
     };
 
     const newProduct = await Product.create(product);
+
     res.writeHead(201, { "Content-Type": "application/json" });
     return res.end(JSON.stringify(newProduct));
   } catch (error) {
